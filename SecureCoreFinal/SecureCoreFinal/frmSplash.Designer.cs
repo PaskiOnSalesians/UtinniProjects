cf466d24d829de199fa270368a174b5186b2801f
oriol <72201508+oriolramisa@users.noreply.github.com>
2021-11-02 19:46:09 +0100

    simple splash & login

---
+++ SecureCoreFinal/SecureCoreFinal/frmSplash.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace SecureCoreFinal
+{
+    partial class frmSplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pgbLoading = new System.Windows.Forms.ProgressBar();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.timerSplash = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pgbLoading
+            // 
+            this.pgbLoading.Location = new System.Drawing.Point(113, 327);
+            this.pgbLoading.Name = "pgbLoading";
+            this.pgbLoading.Size = new System.Drawing.Size(568, 10);
+            this.pgbLoading.TabIndex = 0;
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.Location = new System.Drawing.Point(700, 320);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(28, 17);
+            this.lblLoading.TabIndex = 1;
+            this.lblLoading.Text = "0%";
+            // 
+            // timerSplash
+            // 
+            this.timerSplash.Enabled = true;
+            this.timerSplash.Tick += new System.EventHandler(this.timerSplash_Tick);
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.pgbLoading);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgbLoading;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.Timer timerSplash;
+    }
+}
+
---
+++ SecureCoreFinal/SecureCoreFinal/frmSplash.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecureCoreFinal
+{
+    public partial class frmSplash : Form
+    {
+        public frmSplash()
+        {
+            InitializeComponent();
+        }
+
+        private void timerSplash_Tick(object sender, EventArgs e)
+        {
+            pgbLoading.Increment(4);
+            Opacity -= 0.03;
+            lblLoading.Text = pgbLoading.Value.ToString() + "%";
+
+            if(Opacity == 0)
+            {
+                timerSplash.Stop();
+                this.Hide();
+                frmLogin frmLogin = new frmLogin();
+                frmLogin.ShowDialog();
+            }
+        }
+    }
+}
