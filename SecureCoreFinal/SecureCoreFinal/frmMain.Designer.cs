929a767a779844020b439f327d250e2635f850ae
oriol <72201508+oriolramisa@users.noreply.github.com>
2021-11-05 17:31:37 +0100

    update

--- SecureCoreFinal/SecureCoreFinal/frmMain.Designer.cs
+++ SecureCoreFinal/SecureCoreFinal/frmMain.Designer.cs
@@ -32,31 +32,31 @@ namespace SecureCoreFinal
             this.lblTest = new System.Windows.Forms.Label();
             this.panelTop = new System.Windows.Forms.Panel();
             this.panelLeft = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panelDiv2 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.panelDiv3 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.panelDiv4 = new System.Windows.Forms.Panel();
             this.panel7 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panelDiv4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panelDiv3 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panelDiv2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelTop.SuspendLayout();
             this.panelLeft.SuspendLayout();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.panel5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.panel7.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTest
@@ -99,77 +99,41 @@ namespace SecureCoreFinal
             this.panelLeft.Size = new System.Drawing.Size(200, 492);
             this.panelLeft.TabIndex = 2;
             // 
-            // panel1
+            // panel7
             // 
-            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 75);
-            this.panel1.TabIndex = 1;
+            this.panel7.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel7.Controls.Add(this.label4);
+            this.panel7.Controls.Add(this.pictureBox4);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 309);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(200, 75);
+            this.panel7.TabIndex = 7;
             // 
-            // panelDiv2
+            // label4
             // 
-            this.panelDiv2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelDiv2.Location = new System.Drawing.Point(0, 75);
-            this.panelDiv2.Name = "panelDiv2";
-            this.panelDiv2.Size = new System.Drawing.Size(200, 28);
-            this.panelDiv2.TabIndex = 2;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Funcio 1";
             // 
-            // pictureBox1
+            // pictureBox4
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(22, 17);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox4.Location = new System.Drawing.Point(22, 17);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
             // 
-            // label1
+            // panelDiv4
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Funcio 1";
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.RoyalBlue;
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Controls.Add(this.pictureBox2);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 103);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(200, 75);
-            this.panel3.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(68, 29);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Funcio 1";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(22, 17);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
-            this.pictureBox2.TabIndex = 0;
-            this.pictureBox2.TabStop = false;
-            // 
-            // panelDiv3
-            // 
-            this.panelDiv3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelDiv3.Location = new System.Drawing.Point(0, 178);
-            this.panelDiv3.Name = "panelDiv3";
-            this.panelDiv3.Size = new System.Drawing.Size(200, 28);
-            this.panelDiv3.TabIndex = 4;
+            this.panelDiv4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDiv4.Location = new System.Drawing.Point(0, 281);
+            this.panelDiv4.Name = "panelDiv4";
+            this.panelDiv4.Size = new System.Drawing.Size(200, 28);
+            this.panelDiv4.TabIndex = 6;
             // 
             // panel5
             // 
@@ -199,41 +163,77 @@ namespace SecureCoreFinal
             this.pictureBox3.TabIndex = 0;
             this.pictureBox3.TabStop = false;
             // 
-            // panelDiv4
+            // panelDiv3
             // 
-            this.panelDiv4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelDiv4.Location = new System.Drawing.Point(0, 281);
-            this.panelDiv4.Name = "panelDiv4";
-            this.panelDiv4.Size = new System.Drawing.Size(200, 28);
-            this.panelDiv4.TabIndex = 6;
+            this.panelDiv3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDiv3.Location = new System.Drawing.Point(0, 178);
+            this.panelDiv3.Name = "panelDiv3";
+            this.panelDiv3.Size = new System.Drawing.Size(200, 28);
+            this.panelDiv3.TabIndex = 4;
             // 
-            // panel7
+            // panel3
             // 
-            this.panel7.BackColor = System.Drawing.Color.RoyalBlue;
-            this.panel7.Controls.Add(this.label4);
-            this.panel7.Controls.Add(this.pictureBox4);
-            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel7.Location = new System.Drawing.Point(0, 309);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(200, 75);
-            this.panel7.TabIndex = 7;
+            this.panel3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 103);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 75);
+            this.panel3.TabIndex = 3;
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(68, 29);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 17);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Funcio 1";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Funcio 1";
             // 
-            // pictureBox4
+            // pictureBox2
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(22, 17);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(40, 40);
-            this.pictureBox4.TabIndex = 0;
-            this.pictureBox4.TabStop = false;
+            this.pictureBox2.Location = new System.Drawing.Point(22, 17);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panelDiv2
+            // 
+            this.panelDiv2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDiv2.Location = new System.Drawing.Point(0, 75);
+            this.panelDiv2.Name = "panelDiv2";
+            this.panelDiv2.Size = new System.Drawing.Size(200, 28);
+            this.panelDiv2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 75);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Funcio 1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // frmMain
             // 
@@ -251,18 +251,18 @@ namespace SecureCoreFinal
             this.panelTop.ResumeLayout(false);
             this.panelTop.PerformLayout();
             this.panelLeft.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.panel5.ResumeLayout(false);
-            this.panel5.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.panel7.ResumeLayout(false);
             this.panel7.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
