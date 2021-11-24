69d8aa30d0d5cf1a4c5bcc512e542a5e40719e91
DevPaski <71769140+DevPaski@users.noreply.github.com>
2021-11-24 17:15:50 +0100

    Acces a Dades WIP

---
+++ SecureCoreFinal/AccesDades/Dades.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace AccesDades
+{
+    public class Dades
+    {
+        private string _ConnectionString;
+        public string ConnectionString
+        {
+            get { return _ConnectionString; }
+            set { _ConnectionString = value; }
+        }
+
+
+
+    }
+}
