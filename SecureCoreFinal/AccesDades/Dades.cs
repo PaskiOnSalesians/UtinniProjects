e88389f3d80b80d19d025fc5dca2d9a18286cc7f
DevPaski <71769140+DevPaski@users.noreply.github.com>
2021-11-25 15:41:25 +0100

    Acces a Dades #2

--- SecureCoreFinal/AccesDades/Dades.cs
+++ SecureCoreFinal/AccesDades/Dades.cs
@@ -4,22 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Xml;
+
 using System.Data;
 using System.Data.OleDb;
 using System.Data.SqlClient;
+
+using System.Configuration;
 
 namespace AccesDades
 {
     public class Dades
     {
-        private string _ConnectionString;
+        /*private string _ConnectionString;
         public string ConnectionString
         {
-            get { return _ConnectionString; }
-            set { _ConnectionString = value; }
+            get
+            { 
+                _ConnectionString = 
+                return _ConnectionString;
+            }
+            set { _ConnectionString = ; }
+        }*/
+
+        public void ConnectDB()
+        {
+
         }
-
-
 
     }
 }
