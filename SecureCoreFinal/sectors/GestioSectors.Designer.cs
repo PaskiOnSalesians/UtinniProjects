d9a0ce4e3e0f6f4dbcb2566126bf3bf75bd2f0de
DevPaski <71769140+DevPaski@users.noreply.github.com>
2021-12-20 20:52:54 +0100

    Update Paski
    + Labels
    + Arreglos de connexiones (ahora hay que comentar los codigos de los demás)

--- SecureCoreFinal/sectors/GestioSectors.Designer.cs
+++ SecureCoreFinal/sectors/GestioSectors.Designer.cs
@@ -32,21 +32,25 @@ namespace sectors
             this.swTextbox_code = new InCont.SWTextbox();
             this.swTextbox_remark = new InCont.SWTextbox();
             this.swTextbox_desc = new InCont.SWTextbox();
+            this.lbl_code = new System.Windows.Forms.Label();
+            this.lbl_desc = new System.Windows.Forms.Label();
+            this.lbl_remark = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblExit
             // 
-            this.lblExit.Location = new System.Drawing.Point(953, 11);
+            this.lblExit.Location = new System.Drawing.Point(715, 9);
             // 
             // swTextbox_code
             // 
             this.swTextbox_code.CampBuit = false;
             this.swTextbox_code.ClauForanea = false;
             this.swTextbox_code.DadaPermesa = InCont.SWTextbox.TipusDada.Numero;
-            this.swTextbox_code.Location = new System.Drawing.Point(379, 82);
+            this.swTextbox_code.Location = new System.Drawing.Point(319, 109);
+            this.swTextbox_code.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.swTextbox_code.Name = "swTextbox_code";
             this.swTextbox_code.NomBBDD = "CodeSector";
-            this.swTextbox_code.Size = new System.Drawing.Size(297, 22);
+            this.swTextbox_code.Size = new System.Drawing.Size(224, 20);
             this.swTextbox_code.TabIndex = 8;
             // 
             // swTextbox_remark
@@ -54,10 +58,11 @@ namespace sectors
             this.swTextbox_remark.CampBuit = false;
             this.swTextbox_remark.ClauForanea = false;
             this.swTextbox_remark.DadaPermesa = InCont.SWTextbox.TipusDada.Numero;
-            this.swTextbox_remark.Location = new System.Drawing.Point(379, 177);
+            this.swTextbox_remark.Location = new System.Drawing.Point(319, 186);
+            this.swTextbox_remark.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.swTextbox_remark.Name = "swTextbox_remark";
             this.swTextbox_remark.NomBBDD = "Remarks";
-            this.swTextbox_remark.Size = new System.Drawing.Size(297, 22);
+            this.swTextbox_remark.Size = new System.Drawing.Size(224, 20);
             this.swTextbox_remark.TabIndex = 9;
             // 
             // swTextbox_desc
@@ -65,17 +70,48 @@ namespace sectors
             this.swTextbox_desc.CampBuit = false;
             this.swTextbox_desc.ClauForanea = false;
             this.swTextbox_desc.DadaPermesa = InCont.SWTextbox.TipusDada.Numero;
-            this.swTextbox_desc.Location = new System.Drawing.Point(379, 128);
+            this.swTextbox_desc.Location = new System.Drawing.Point(319, 146);
+            this.swTextbox_desc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.swTextbox_desc.Name = "swTextbox_desc";
             this.swTextbox_desc.NomBBDD = "DescSector";
-            this.swTextbox_desc.Size = new System.Drawing.Size(297, 22);
+            this.swTextbox_desc.Size = new System.Drawing.Size(224, 20);
             this.swTextbox_desc.TabIndex = 10;
+            // 
+            // lbl_code
+            // 
+            this.lbl_code.AutoSize = true;
+            this.lbl_code.Location = new System.Drawing.Point(202, 112);
+            this.lbl_code.Name = "lbl_code";
+            this.lbl_code.Size = new System.Drawing.Size(66, 13);
+            this.lbl_code.TabIndex = 11;
+            this.lbl_code.Text = "CodeSector:";
+            // 
+            // lbl_desc
+            // 
+            this.lbl_desc.AutoSize = true;
+            this.lbl_desc.Location = new System.Drawing.Point(202, 149);
+            this.lbl_desc.Name = "lbl_desc";
+            this.lbl_desc.Size = new System.Drawing.Size(66, 13);
+            this.lbl_desc.TabIndex = 12;
+            this.lbl_desc.Text = "DescSector:";
+            // 
+            // lbl_remark
+            // 
+            this.lbl_remark.AutoSize = true;
+            this.lbl_remark.Location = new System.Drawing.Point(202, 189);
+            this.lbl_remark.Name = "lbl_remark";
+            this.lbl_remark.Size = new System.Drawing.Size(52, 13);
+            this.lbl_remark.TabIndex = 13;
+            this.lbl_remark.Text = "Remarks:";
             // 
             // GestioSectors
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 624);
+            this.ClientSize = new System.Drawing.Size(753, 507);
+            this.Controls.Add(this.lbl_remark);
+            this.Controls.Add(this.lbl_desc);
+            this.Controls.Add(this.lbl_code);
             this.Controls.Add(this.swTextbox_desc);
             this.Controls.Add(this.swTextbox_remark);
             this.Controls.Add(this.swTextbox_code);
@@ -86,6 +122,9 @@ namespace sectors
             this.Controls.SetChildIndex(this.swTextbox_code, 0);
             this.Controls.SetChildIndex(this.swTextbox_remark, 0);
             this.Controls.SetChildIndex(this.swTextbox_desc, 0);
+            this.Controls.SetChildIndex(this.lbl_code, 0);
+            this.Controls.SetChildIndex(this.lbl_desc, 0);
+            this.Controls.SetChildIndex(this.lbl_remark, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,5 +135,8 @@ namespace sectors
         private InCont.SWTextbox swTextbox_code;
         private InCont.SWTextbox swTextbox_remark;
         private InCont.SWTextbox swTextbox_desc;
+        private System.Windows.Forms.Label lbl_code;
+        private System.Windows.Forms.Label lbl_desc;
+        private System.Windows.Forms.Label lbl_remark;
     }
 }
