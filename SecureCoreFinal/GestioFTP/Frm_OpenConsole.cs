a36388b7972e071d3372ac004a4aafadd873cadd
DevPaski <71769140+DevPaski@users.noreply.github.com>
2022-02-08 19:10:12 +0100

    Update con cosas rotas

---
+++ SecureCoreFinal/GestioFTP/Frm_OpenConsole.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestioFTP
+{
+    public partial class Frm_OpenConsole : Form
+    {
+        string path;
+        public Frm_OpenConsole()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_console_Click(object sender, EventArgs e)
+        {
+            Process.Start(path + "\\ConsolaFTP\\bin");
+        }
+
+        private void Frm_OpenConsole_Load(object sender, EventArgs e)
+        {
+            path = Application.StartupPath;
+        }
+    }
+}
---
+++ SecureCoreFinal/OpenConsole/frm_console.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace OpenConsole
+{
+    partial class frm_console
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_close = new System.Windows.Forms.Label();
+            this.btn_console = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_close
+            // 
+            this.lbl_close.AutoSize = true;
+            this.lbl_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_close.Location = new System.Drawing.Point(749, 19);
+            this.lbl_close.Name = "lbl_close";
+            this.lbl_close.Size = new System.Drawing.Size(39, 37);
+            this.lbl_close.TabIndex = 0;
+            this.lbl_close.Text = "X";
+            this.lbl_close.Click += new System.EventHandler(this.lbl_close_Click);
+            // 
+            // btn_console
+            // 
+            this.btn_console.BackColor = System.Drawing.Color.Black;
+            this.btn_console.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_console.ForeColor = System.Drawing.Color.White;
+            this.btn_console.Location = new System.Drawing.Point(237, 154);
+            this.btn_console.Name = "btn_console";
+            this.btn_console.Size = new System.Drawing.Size(282, 176);
+            this.btn_console.TabIndex = 1;
+            this.btn_console.Text = "Open Console";
+            this.btn_console.UseVisualStyleBackColor = false;
+            this.btn_console.Click += new System.EventHandler(this.btn_console_Click);
+            // 
+            // frm_console
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_console);
+            this.Controls.Add(this.lbl_close);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_console";
+            this.Text = "OpenConsole";
+            this.Load += new System.EventHandler(this.frm_console_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_close;
+        private System.Windows.Forms.Button btn_console;
+    }
+}
+
