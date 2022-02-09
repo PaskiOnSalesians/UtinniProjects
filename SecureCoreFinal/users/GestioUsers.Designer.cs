4d1cd926e1b0a2079b37889264d66c2eab9cad25
Gerard <91083698+Raips@users.noreply.github.com>
2022-02-09 16:04:17 +0100

    meh

--- SecureCoreFinal/users/GestioUsers.Designer.cs
+++ SecureCoreFinal/users/GestioUsers.Designer.cs
@@ -43,17 +43,19 @@ namespace users
             // 
             // lblExit
             // 
-            this.lblExit.Location = new System.Drawing.Point(729, 9);
+            this.lblExit.Location = new System.Drawing.Point(1094, 14);
+            this.lblExit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             // 
             // swTextbox_codi
             // 
             this.swTextbox_codi.CampBuit = false;
             this.swTextbox_codi.ClauForanea = false;
             this.swTextbox_codi.DadaPermesa = InCont.SWTextbox.TipusDada.Codi;
-            this.swTextbox_codi.Location = new System.Drawing.Point(329, 100);
+            this.swTextbox_codi.Location = new System.Drawing.Point(494, 154);
+            this.swTextbox_codi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.swTextbox_codi.Name = "swTextbox_codi";
             this.swTextbox_codi.NomBBDD = "CodeUser";
-            this.swTextbox_codi.Size = new System.Drawing.Size(182, 20);
+            this.swTextbox_codi.Size = new System.Drawing.Size(271, 26);
             this.swTextbox_codi.TabIndex = 8;
             // 
             // swTextbox_desc
@@ -61,10 +63,11 @@ namespace users
             this.swTextbox_desc.CampBuit = false;
             this.swTextbox_desc.ClauForanea = false;
             this.swTextbox_desc.DadaPermesa = InCont.SWTextbox.TipusDada.Text;
-            this.swTextbox_desc.Location = new System.Drawing.Point(329, 126);
+            this.swTextbox_desc.Location = new System.Drawing.Point(494, 194);
+            this.swTextbox_desc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.swTextbox_desc.Name = "swTextbox_desc";
             this.swTextbox_desc.NomBBDD = "UserName";
-            this.swTextbox_desc.Size = new System.Drawing.Size(182, 20);
+            this.swTextbox_desc.Size = new System.Drawing.Size(271, 26);
             this.swTextbox_desc.TabIndex = 9;
             // 
             // swTextbox_password
@@ -72,10 +75,11 @@ namespace users
             this.swTextbox_password.CampBuit = false;
             this.swTextbox_password.ClauForanea = false;
             this.swTextbox_password.DadaPermesa = InCont.SWTextbox.TipusDada.Text;
-            this.swTextbox_password.Location = new System.Drawing.Point(329, 178);
+            this.swTextbox_password.Location = new System.Drawing.Point(494, 274);
+            this.swTextbox_password.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.swTextbox_password.Name = "swTextbox_password";
             this.swTextbox_password.NomBBDD = "Password";
-            this.swTextbox_password.Size = new System.Drawing.Size(182, 20);
+            this.swTextbox_password.Size = new System.Drawing.Size(271, 26);
             this.swTextbox_password.TabIndex = 11;
             // 
             // swTextbox_login
@@ -83,10 +87,11 @@ namespace users
             this.swTextbox_login.CampBuit = false;
             this.swTextbox_login.ClauForanea = false;
             this.swTextbox_login.DadaPermesa = InCont.SWTextbox.TipusDada.Text;
-            this.swTextbox_login.Location = new System.Drawing.Point(329, 152);
+            this.swTextbox_login.Location = new System.Drawing.Point(494, 234);
+            this.swTextbox_login.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.swTextbox_login.Name = "swTextbox_login";
             this.swTextbox_login.NomBBDD = "Login";
-            this.swTextbox_login.Size = new System.Drawing.Size(182, 20);
+            this.swTextbox_login.Size = new System.Drawing.Size(271, 26);
             this.swTextbox_login.TabIndex = 10;
             // 
             // swTextbox_photo
@@ -94,62 +99,68 @@ namespace users
             this.swTextbox_photo.CampBuit = false;
             this.swTextbox_photo.ClauForanea = false;
             this.swTextbox_photo.DadaPermesa = InCont.SWTextbox.TipusDada.Text;
-            this.swTextbox_photo.Location = new System.Drawing.Point(329, 204);
+            this.swTextbox_photo.Location = new System.Drawing.Point(494, 314);
+            this.swTextbox_photo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.swTextbox_photo.Name = "swTextbox_photo";
             this.swTextbox_photo.NomBBDD = "Photo";
-            this.swTextbox_photo.Size = new System.Drawing.Size(182, 20);
+            this.swTextbox_photo.Size = new System.Drawing.Size(271, 26);
             this.swTextbox_photo.TabIndex = 12;
             // 
             // lbl_codi
             // 
             this.lbl_codi.AutoSize = true;
-            this.lbl_codi.Location = new System.Drawing.Point(217, 103);
+            this.lbl_codi.Location = new System.Drawing.Point(326, 158);
+            this.lbl_codi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_codi.Name = "lbl_codi";
-            this.lbl_codi.Size = new System.Drawing.Size(31, 13);
+            this.lbl_codi.Size = new System.Drawing.Size(45, 20);
             this.lbl_codi.TabIndex = 13;
             this.lbl_codi.Text = "Codi:";
             // 
             // lbl_user
             // 
             this.lbl_user.AutoSize = true;
-            this.lbl_user.Location = new System.Drawing.Point(217, 129);
+            this.lbl_user.Location = new System.Drawing.Point(326, 198);
+            this.lbl_user.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_user.Name = "lbl_user";
-            this.lbl_user.Size = new System.Drawing.Size(58, 13);
+            this.lbl_user.Size = new System.Drawing.Size(87, 20);
             this.lbl_user.TabIndex = 14;
             this.lbl_user.Text = "Username:";
             // 
             // lbl_login
             // 
             this.lbl_login.AutoSize = true;
-            this.lbl_login.Location = new System.Drawing.Point(217, 155);
+            this.lbl_login.Location = new System.Drawing.Point(326, 238);
+            this.lbl_login.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_login.Name = "lbl_login";
-            this.lbl_login.Size = new System.Drawing.Size(36, 13);
+            this.lbl_login.Size = new System.Drawing.Size(52, 20);
             this.lbl_login.TabIndex = 15;
             this.lbl_login.Text = "Login:";
             // 
             // lbl_password
             // 
             this.lbl_password.AutoSize = true;
-            this.lbl_password.Location = new System.Drawing.Point(217, 181);
+            this.lbl_password.Location = new System.Drawing.Point(326, 278);
+            this.lbl_password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_password.Name = "lbl_password";
-            this.lbl_password.Size = new System.Drawing.Size(56, 13);
+            this.lbl_password.Size = new System.Drawing.Size(82, 20);
             this.lbl_password.TabIndex = 16;
             this.lbl_password.Text = "Password:";
             // 
             // lbl_photo
             // 
             this.lbl_photo.AutoSize = true;
-            this.lbl_photo.Location = new System.Drawing.Point(217, 207);
+            this.lbl_photo.Location = new System.Drawing.Point(326, 318);
+            this.lbl_photo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_photo.Name = "lbl_photo";
-            this.lbl_photo.Size = new System.Drawing.Size(38, 13);
+            this.lbl_photo.Size = new System.Drawing.Size(55, 20);
             this.lbl_photo.TabIndex = 17;
             this.lbl_photo.Text = "Photo:";
             // 
             // GestioUsers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(767, 485);
+            this.ClientSize = new System.Drawing.Size(1150, 962);
             this.Controls.Add(this.lbl_photo);
             this.Controls.Add(this.lbl_password);
             this.Controls.Add(this.lbl_login);
@@ -160,6 +171,7 @@ namespace users
             this.Controls.Add(this.swTextbox_login);
             this.Controls.Add(this.swTextbox_desc);
             this.Controls.Add(this.swTextbox_codi);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "GestioUsers";
             this.Text = "GestioUsers";
             this.Controls.SetChildIndex(this.lblExit, 0);
