14363029c662044b7aca06785479a828f08fdf5f
DevPaski <71769140+DevPaski@users.noreply.github.com>
2022-05-12 19:57:26 +0200

    Alguna cosa arreglada

--- FullProject/ConsoleForm/OpenConsole.cs
+++ FullProject/ConsoleForm/OpenConsole.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+
 namespace ConsoleForm
 {
     public partial class OpenConsole : Form
@@ -23,12 +25,20 @@ namespace ConsoleForm
         private void btn_console_Click(object sender, EventArgs e)
         {
             //Process.Start(path + "\\dll\\ConsolaFTP.exe");
-            Process.Start(@"C:\\Users\\ribes\\Desktop\\projectefinal\\Utinni-Projectes\\SecureCoreFinal\\dll\\ConsolaFTP.exe");
+
+            string consolaftp = path + "\\ConsolaFTP.exe";
+
+            Console.WriteLine(consolaftp);
+
+            //Process.Start(Path.GetFullPath(consolaftp)); // @"C:\\Users\\ribes\\Desktop\\projectefinal\\Utinni-Projectes\\SecureCoreFinal"
+            Process.Start(consolaftp); // @"C:\\Users\\ribes\\Desktop\\projectefinal\\Utinni-Projectes\\SecureCoreFinal"
+            
         }
 
         private void OpenConsole_Load(object sender, EventArgs e)
         {
             path = Application.StartupPath;
+            Console.WriteLine(path);
         }
 
         private void lblExit_Click(object sender, EventArgs e)
